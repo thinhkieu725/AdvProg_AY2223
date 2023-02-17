f34754b26a4da41f6b78d81c40d2029f9149efb6
thinhkieu725 <thinhkieu725@gmail.com>
2023-02-17 10:15:06 +0700

    attempt_2_guessIt

--- AdvProg_L1-GuessIt/guessit.cpp
+++ AdvProg_L1-GuessIt/guessit.cpp
@@ -48,9 +48,9 @@ string getAnswer(int number, int randomNumber) {
               If number is equal randomNumber, the answer is "Congratulation! You win."
     ***/
     string answer;
-    if (guess > secretNumber)
+    if (number > secretNumber)
         answer = "Your number is higher.";
-    else if (guess < secretNumber)
+    else if (number < secretNumber)
         answer = "Your number is lower";
     else
         answer = "Congratulation! You win!";
